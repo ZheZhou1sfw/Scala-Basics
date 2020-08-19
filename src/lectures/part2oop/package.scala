2ea3641dcd63618940fafe3f40d816c3b9b358df
unknown <zhezhou@gatech.edu>
2020-08-19 14:50:52 -0400

    learned Packaging and Imports

---
+++ src/lectures/part2oop/package.scala
@@ -0,0 +1,8 @@
+package lectures
+
+// This ia package object -- only one per package
+package object part2oop {
+
+  def sayHello: Unit = println("Hello, Scala")
+  val SPEED_OF_LIGHT = 299792485
+}
