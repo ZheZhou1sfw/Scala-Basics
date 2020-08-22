d3638617fa94f454cde35f66a4e8d953aebbcfb9
unknown <zhezhou@gatech.edu>
2020-08-22 10:47:43 -0400

    hofs and curries exercise

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -11,13 +11,16 @@ abstract class MyList[+A] {
   def map[B](transformer: A => B): MyList[B]
   def filter(predicate: A => Boolean): MyList[A]
   def flatMap[B](transformer: A => MyList[B]): MyList[B]
+  def foreach(f: A => Unit): Unit
+  def sort(compare: (A, A) => Int): MyList[A]
+  def zipWith[B, C](list: MyList[B], zip:(A, B)=> C): MyList[C]
+  def fold[B](start: B)(operator: (B, A) => B): B
+
 
   override def toString: String = "[" + printElements + "]"
 
   def ++[B >: A](list: MyList[B]): MyList[B]
 }
-
-
 
 
 // object can extend classes
@@ -32,11 +35,22 @@ case object Empty extends MyList[Nothing] {
 
   override def printElements: String = ""
 
-  def map[B](transformer: Nothing => B): MyList[B] = Empty
+  override def map[B](transformer: Nothing => B): MyList[B] = Empty
 
-  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+  override def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
-  def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+  override def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+
+  // hofs
+  override def foreach(f: Nothing => Unit): Unit = () // return the unit value
+
+  override def sort(compare: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
+
+  override def zipWith[B, C](list: MyList[B], zip: (Nothing, B) => C): MyList[C] =
+    if (!list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else Empty
+
+  override def fold[B](start: B)(operator: (B, Nothing) => B): B = start
 
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
@@ -54,22 +68,48 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     if (t.isEmpty) "" + h
     else h + " " + t.printElements
 
-  def filter(predicate: A => Boolean): MyList[A] =
+  override def filter(predicate: A => Boolean): MyList[A] =
     if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
 
-  def map[B](transformer: A => B): MyList[B] =
+  override def map[B](transformer: A => B): MyList[B] =
     new Cons(transformer(h), t.map(transformer))
-
-  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 
   override def flatMap[B](transformer: A => MyList[B]): MyList[B] =
     transformer(h) ++ t.flatMap(transformer)
+
+  override def foreach(f: A => Unit): Unit = {
+    f(h)
+    t.foreach(f)
+  }
+
+  override def sort(compare: (A, A) => Int): MyList[A] = {
+
+    def insert(x: A, sortedList: MyList[A]): MyList[A] = {
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+    }
+
+    val sortedTail = t.sort(compare)
+    insert(h, sortedTail)
+  }
+
+  override def zipWith[B, C](list: MyList[B], zip: (A, B) => C): MyList[C] =
+    if (list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else new Cons(zip(h, list.head), t.zipWith(list.tail, zip))
+
+  override def fold[B](start: B)(operator: (B, A) => B): B =
+    t.fold( operator(start, h))(operator)
+
+
+
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 }
 
 object ListTest extends App {
   val listOfIntegers: MyList[Int] = new Cons(1, new Cons[Int](2, new Cons[Int](4, Empty)))
-  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons[Int](5, new Cons[Int](6, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons[Int](5, Empty))
   val listOfStrings: MyList[String] = new Cons("Hello", new Cons[String]("Scala", Empty))
 
   println(listOfIntegers).toString
@@ -81,4 +121,9 @@ object ListTest extends App {
 
   println((listOfIntegers ++ anotherListOfIntegers))
   println(listOfIntegers.flatMap((elem: Int) => new Cons[Int](elem, new Cons(elem + 1, Empty))))
+
+  listOfIntegers.foreach(println)
+  println(listOfIntegers.sort((x, y) => y - x))
+  println(anotherListOfIntegers.zipWith[String, String](listOfStrings, _ + "-" + _))
+  println(listOfIntegers.fold(0)(_ + _)) // reduce
 }
